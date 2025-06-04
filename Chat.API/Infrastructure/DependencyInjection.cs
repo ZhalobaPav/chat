eb4606cba924e6d41662ff8307352a34a8e82ed2
ZhalobaPav <zhalobapavlo@gmail.com>
2025-06-04 21:20:42 +0300

    infrastructure adjustments

---
+++ Chat.API/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ChatDbContext>((options) =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
