1434476d667f21076cc5e7bfba2c67c6a8df2986
ZhalobaPav <zhalobapavlo@gmail.com>
2025-06-04 21:01:35 +0300

    DbContext4

---
+++ Chat.API/Infrastructure/Data/ChatDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ChatDbContext: IdentityDbContext<ChatUser>
+    {
+        public ChatDbContext(DbContextOptions<ChatDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
